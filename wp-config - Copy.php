ee016bc0adbe300ea9d15a44f27bbd700d199ca6
arielZusman <ariel.zusman@gmail.com>
2013-08-19 19:39:16 +0300

    changed config file

--- wp-config - Copy.php
+++ wp-config - Copy.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'hila');
+define('DB_NAME', 'wp_hila');
 
 /** MySQL database username */
-define('DB_USER', 'daria');
+define('DB_USER', 'admin');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'ZL2bY8xQ');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'tunnel.pagodabox.com:3306');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
